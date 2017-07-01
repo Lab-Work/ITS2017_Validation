8ddf2bfdb4008644089717db39249b941476ba6a
Fangyu Wu <fywu85@gmail.com>
2017-07-01 14:53:42 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from TrafficEstimator import TrafficEstimator
 vc = TrafficEstimator("./", "sample.mp4", 60.0, [10,-10],
                       [400, 345, 332], [0.00, 3.66, 7.32], 0.4669, 0.0, 4.27)
 # Find the heat map/heat trace of the video.
-#vc.find_trace()
+vc.find_trace()
 # Estimate vehicle counts, distance, and speeds using DBSCAN and convex hull.
 vc.analyze_trace()
 # Visualize the estimation results in form of histogram and video overlay.
